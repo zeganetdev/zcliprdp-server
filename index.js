21dfc518867ea4955e29cb9a32de59618bd5f05e
Edgard <zeganet@gmail.com>
2021-12-19 00:36:26 -0500

    agregar raiz /

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require("socket.io")(http);
 
+app.get('/', (req, res) => {
+    res.send("run run ...");
+});
+
 app.get('/test', (req, res) => {
     res.sendFile(__dirname + '/test.html');
 });
