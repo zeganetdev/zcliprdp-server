db018bdadcc66a029e90277a56ac5c7dd3eac51a
Edgard <zeganet@gmail.com>
2021-12-20 10:01:06 -0500

    reducido

--- index.js
+++ index.js
@@ -2,31 +2,12 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.send("run run ...");
-});
 
-app.get('/test', (req, res) => {
-    res.sendFile(__dirname + '/test.html');
-});
+app.get('/', (req, res) => res.send("run run ... <br>zeganet") );
 
 io.on('connection', async socket => {
-    socket.on('addUser', (obj) => {
-        socket.join(obj.UserName);
-        console.log('addUser: ' + obj.UserName);
-    }); 
-
-    socket.on('messagePrivate', obj => {
-        console.log('messagePrivate: ', obj.Format);
-        socket.to(obj.UserName).emit("messagePrivate", obj);
-    });
-
-    socket.on('disconnect', () => {
-      console.log('disconnect');
-    });
-    
+    socket.on('addUser', (obj) => socket.join(obj.UserName) ); 
+    socket.on('messagePrivate', obj => socket.to(obj.UserName).emit("messagePrivate", obj) );
 });
 
-http.listen(port, () => {
-    console.log('init');
-});
+http.listen(port, () => console.log(`Init port: ${port}`));
